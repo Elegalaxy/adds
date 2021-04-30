c9428d27efac1b5b3af6d2252fdee39492d3ea31
Elegalaxy <yokai.czk@gmail.com>
2021-04-30 09:48:32 +0930

    update2

--- Rearrange.cpp
+++ Rearrange.cpp
@@ -1,12 +1,17 @@
 #include "Rearrange.h"
 #include <string>
-
+#include <iostream>
 Rearrange::Rearrange(){
 
 }
 
 Individual* Rearrange::mutate(Individual* i, int k){
     if(k <= 0) return i;
+
+    int n = i->getLength();
+    if(k%n == 0) k = k-n*(k/n-1);
+    else k = k%n;
+
     std::string temp = i->getString();
     temp = temp.substr(k-1) + temp.substr(0, k-1);
     delete(i);
