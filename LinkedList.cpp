23bd7f17d89c937f0e4373805dcdcb5de8622f47
Elegalaxy <yokai.czk@gmail.com>
2021-05-09 01:47:39 +0930

    update prac 8.2

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -30,29 +30,79 @@ void LinkedList::addEnd(int newItem){
 }
 
 void LinkedList::addAtPosition(int position, int newItem){
-    for(int i = 0; i < position-1; i++){
-        
+    Node* temp = head;
+    if(position == 1){
+        addFront(newItem);
+        return;
     }
+
+    for(int i = 0; i < position-2; i++){
+        temp = temp->getNext();
+        if(!temp) return;
+    }
+
+    Node* nxt = temp->getNext();
+    temp->setNext(new Node(newItem));
+    temp->getNext()->setNext(nxt);
 }
 
 int LinkedList::search(int item){
-
+    Node* temp = head;
+    int i = 1;
+    while(temp){
+        if(temp->getData() == item){
+            std::cout << i << " ";
+            return i;
+        }
+        temp = temp->getNext();
+        i++;
+    }
+    return 0;
 }
 
 void LinkedList::deleteFront(){
-
+    Node* temp = head;
+    head = head->getNext();
+    delete(temp);
 }
 
 void LinkedList::deleteEnd(){
-
+    Node* temp = head;
+    while(temp->getNext()){
+        temp = temp->getNext();
+    }
+    delete(temp->getNext());
+    temp->setNext(nullptr);
 }
 
 void LinkedList::deletePosition(int position){
+    if(position <= 0)
+        return;
 
+    if(position == 1){
+        deleteFront();
+        return;
+    }
+
+    Node* temp = head;
+    for(int i = 0; i < position-2; i++){
+        head = head->getNext();
+        if(!head)
+            return;
+    }
+    Node* t = temp->getNext();
+    temp->setNext(temp->getNext()->getNext());
+    delete(t);
 }
 
 int LinkedList::getItem(int position){
-
+    Node* temp = head;
+    for(int i = 0; i < position; i++){
+        temp = temp->getNext();
+        if(!temp)
+            return;
+    }
+    std::cout << temp->getData() << " ";
 }
 
 void LinkedList::printItems(){
--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include "LinkedList.h"
 using namespace std;
 
 int main(){
+    vector<int> vec = {1,2,3};
+    LinkedList* list = new LinkedList(vec, 3);
+
+    list->addFront(0);
+    list->printItems();
+    list->addEnd(4);
+    list->printItems();
+    list->addAtPosition(2, 10);
+    list->printItems();
+
+    list->search(2);
+    list->search(20);
+
+    list->deleteFront();
+    list->printItems();
+    list->deleteEnd();
+    list->printItems();
+    list->deletePosition(3);
+    list->printItems();
 
     return 0;
 }
