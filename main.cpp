f5cd9bedbdb9286bff937922edc1787670c03ccf
Elegalaxy <yokai.czk@gmail.com>
2021-05-25 21:45:17 +0930

    update

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@ int main(){
     string s;
     getline(cin, s);
 
+    if(s == " " || s.length() == 0){
+        cout << "Error" << endl;
+        return 0;
+    }
     Infix* infix = new Infix(s);
     infix->convert();
     return 0;
